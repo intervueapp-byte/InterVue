7974b3c68493d55653ff4f87517f377f68615223
Raj Posture <YOUR_GITHUB_PRIMARY_EMAIL>
2026-04-02 20:45:03 +0530

    axios for bith production and local testing ready

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 const axiosInstance = axios.create({
-  baseURL: "https://intervue-t8xv.onrender.com", 
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
